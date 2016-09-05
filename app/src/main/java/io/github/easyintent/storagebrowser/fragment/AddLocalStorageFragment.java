900b444b630807fece4c2330eef4a6c4cbf966bd
Bangun K <bangun@users.noreply.github.com>
2016-09-05 14:08:17 +0700

    support arbitrary location for local storage

--- app/src/main/java/io/github/easyintent/storagebrowser/fragment/AddLocalStorageFragment.java
+++ app/src/main/java/io/github/easyintent/storagebrowser/fragment/AddLocalStorageFragment.java
@@ -5,8 +5,12 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
@@ -19,28 +23,58 @@ import io.github.easyintent.storagebrowser.R;
 public class AddLocalStorageFragment extends DialogFragment {
 
     @ViewById protected EditText nameView;
-    @ViewById protected EditText subDirView;
+    @ViewById protected EditText anyDirView;
+
+    @ViewById protected RadioButton advancedView;
+    @ViewById protected RadioButton sharedStorageView;
+    @ViewById protected View advancedGroup;
 
     public static AddLocalStorageFragment newInstance() {
         return new AddLocalStorageFragmentEx();
     }
 
+    @AfterViews
+    protected void afterViews() {
+        if (anyDirView.getText().length() == 0) {
+            anyDirView.setText(Environment.getExternalStorageDirectory().getAbsolutePath());
+        }
+        advancedView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                advancedGroup.setVisibility(advancedView.isChecked() ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
     @Click
     protected void saveButtonClicked() {
         String name = nameView.getText().toString();
-        String subDir = subDirView.getText().toString();
+        String subDir = anyDirView.getText().toString();
         if (name.isEmpty()) {
             nameView.setError(getString(R.string.lbl_required));
             return;
         }
 
-        File root = Environment.getExternalStorageDirectory();
-        File target = new File(root, subDir);
-        if (!(target.exists() && target.isDirectory())) {
-            subDirView.setError(getString(R.string.msg_location_does_not_exist));
-            return;
+        if (sharedStorageView.isChecked()) {
+            File root = Environment.getExternalStorageDirectory();
+            useLocation(name, root);
         }
 
+        if (advancedView.isChecked()) {
+            addAnyPath(name, subDir);
+        }
+    }
+
+    private void addAnyPath(String name, String subDir) {
+        File target = new File(subDir);
+        if ((target.exists() && target.isDirectory())) {
+            useLocation(name, target);
+            return;
+        }
+        anyDirView.setError(getString(R.string.msg_location_does_not_exist));
+    }
+
+    private void useLocation(String name, File target) {
         Intent intent = new Intent();
         intent.setData(Uri.fromFile(target));
         intent.putExtra(Intent.EXTRA_TITLE, name);
